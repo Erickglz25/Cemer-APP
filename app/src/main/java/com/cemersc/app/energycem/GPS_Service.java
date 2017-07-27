fc13df0cbb5b82ad970b5c13c384eba73d960a80
Erickglz25 <Erick.glz25@gmail.com>
2017-07-27 18:37:50 -0500

    Add intent-solar

---
+++ app/src/main/java/com/cemersc/app/energycem/GPS_Service.java
@@ -0,0 +1,81 @@
+package com.cemersc.app.energycem;
+
+/**
+ * Created by erick on 27/07/17.
+ */
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+
+
+public class GPS_Service extends Service {
+
+    private LocationListener listener;
+    private LocationManager locationManager;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {//cada vez que las coordenadas cambian
+                //se llama este metodo, se genera un nuevo intent, co el filtro location_update
+                Intent i = new Intent("location_update");
+                i.putExtra("coordinates",location.getLongitude()+" "+location.getLatitude());
+                sendBroadcast(i);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            }
+        };
+
+        locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+
+        //noinspection MissingPermission
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,3000,0,listener);
+        /*provider	the name of the provider with which we would like to regiser.
+minTime	minimum time interval between location updates (in milliseconds).
+minDistance	minimum distance between location updates (in meters).
+listener	a LocationListener whose onLocationChanged(Location) method will be called for each location update.*/
+
+    }
+
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(locationManager != null){
+            //noinspection MissingPermission
+            locationManager.removeUpdates(listener);
+        }
+    }
+}
--- app/src/main/java/com/cemersc/app/energycem/MainActivity.java
+++ app/src/main/java/com/cemersc/app/energycem/MainActivity.java
@@ -1,13 +1,33 @@
 package com.cemersc.app.energycem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ImageView Solarbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Solarbtn = (ImageView) findViewById(R.id.imageView9);
+        Solarbtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.imageView9:
+                Intent i_solar = new Intent(MainActivity.this,SolarActivity.class);
+                startActivity(i_solar);
+                break;
+        }
+
     }
 }
